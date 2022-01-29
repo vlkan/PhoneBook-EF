a51fb29fae75262fe442aefbdd62672e6782a3f9
vlkan <thkvolkanonder@gmail.com>
2022-01-29 21:07:06 +0300

    IoC usage

---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //attribute
+    public class CustomersController : ControllerBase
+    {
+        ICustomerService _customerService;
+        //loosely coupled - gevşek bağımlılık.
+        //IoC Container -- Inversion of Control working like box. new in memory. Adressed using which class
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public List<Customer> Get() {
+            var result = _customerService.GetAll();
+            return result.Data;
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Business.Abstract;
+using Business.Concrete;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICustomerService, CustomerManager>(); //when call service, create new manager
 
 var app = builder.Build();
 
